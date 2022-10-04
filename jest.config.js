c489810c67668ea5a97446859e4f9e6d01c3698a
lucas.borges <lucas.borges@tegra.com.br>
2022-10-04 16:20:51 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
 
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'V8',
+  coverageDirectory: './coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
